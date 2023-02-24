b62daeac34db840bbce968bc3e83388b77296b04
Hellhow_mode83 <sirjacques.vinc@gmail.com>
2023-02-24 13:22:59 +0100

    bouton de démo

--- js/main2.js
+++ js/main2.js
@@ -1,4 +1,5 @@
-// ANCHOR list des let
+// SECTION variable
+// ANCHOR élément du DOM
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
@@ -6,7 +7,11 @@ let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
+// ANCHOR DB temporaire
 let data = [];
+// ANCHOR pour les fct pour des dates
+let date = new Date();
+// !SECTION variable
 
 // ANCHOR fct d'écoute au submit
 form.addEventListener("submit", (e) => {
@@ -104,13 +109,78 @@ let editTask = () => {
     createTasks();
 })();
 
-// ANCHOR fct reste all tasks
+// ANCHOR fct écoute de dellAll
 let suppr = document.querySelector('.btn.btn-danger')
 suppr.addEventListener(`click`, () => {
+    dellAll();
+});
+
+// ANCHOR fct reste all tasks
+let dellAll = () => {
     let modifs = document.querySelectorAll('.fa-edit');
     modifs.forEach((modif) => {
         data = [];
         localStorage.setItem("data", JSON.stringify(data));
         modif.parentElement.parentElement.remove();
     });
-});
+}
+
+// ANCHOR fct demo
+let demo = document.querySelector(`#btn__demo`);
+demo.addEventListener(`click`, () => {
+    dellAll();
+    for (let i = 1; i < 5; i++) {
+        txtRadom();
+        localStorage.setItem("data", JSON.stringify(data));
+        console.log(data);
+        createTasks();
+    }
+})
+
+// ANCHOR fct radome 5 (Dès à 5 faces)
+let D5 = () => {
+    return (Math.floor(Math.random() * 5 + 1));
+}
+
+// ANCHOR fct text radome
+let txtRadom = () => {
+    switch (D5()) {
+        case 1:
+            data.push({
+                text: `Tache exemple`,
+                date: date.toLocaleDateString(),
+                description: `Description de la tache en question`,
+            });
+            break;
+        case 2:
+            data.push({
+                text: `MARTI !`,
+                date: `05/09/1955`,
+                description: `Il faut que tu retourne dans le future le 5 novembre 1995`,
+            });
+            break;
+        case 3:
+            data.push({
+                text: `Changer les boutons`,
+                date: `01/04/${date.getFullYear()}`,
+                description: `Il faut changer le CSS des boutons pour le projet Easter Eggs.`,
+            });
+            break;
+        case 4:
+            data.push({
+                text: `Test`,
+                date: date.toLocaleDateString(),
+                description: `Test de description`,
+            });
+            break;
+        case 5:
+            data.push({
+                text: `Bonjour le monde`,
+                date: `00/05/1996`,
+                description: `Les premier pas de JavaScrip`,
+            });
+            break;
+        default:
+            break;
+    }
+}
